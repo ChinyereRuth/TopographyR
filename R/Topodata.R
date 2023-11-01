7cd541172520ace8eba4b6986b08817059c304d6
Chinyere Ottah <ottahc@mcmaster.ca>
2023-11-01 11:13:16 -0400

    I created a topdara for R in R script to help with documentaion

---
+++ R/Topodata.R
@@ -0,0 +1,45 @@
+' Calculate Slope and Aspect from a Digital Elevation Model (DEM)
+#'
+#' This function computes both slope and aspect from a Digital Elevation Model (DEM).
+#'
+#' @param dem A raster object representing the Digital Elevation Model.
+#' @return A list containing two raster objects: "slope" representing the slope and "aspect" representing the aspect.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Example usage
+#' elevation_raster <- raster("/Users/chinyereottah/Desktop/Mac/SpatialR/data-raw/CanDEM.tif")
+#' result <- calculate_slope_aspect(elevation_raster)
+#' slope_raster <- result$slope
+#' aspect_raster <- result$aspect
+#' plot(slope_raster, main="Slope")
+#' plot(aspect_raster, main="Aspect")
+#' }
+#'
+#' @seealso \code{\link[raster]{focal}}, \code{\link[raster]{atan}}, \code{\link[base]{atan2}}
+#'
+#' @import raster
+#' @importFrom raster focal
+#' @importFrom raster atan
+#' @importFrom base atan2
+#'
+#' @author Chinyere Ottah
+#'
+#' @keywords spatial
+#'
+#' @examples
+#' \dontrun{
+#' # Example usage
+#' elevation_raster <- raster("/Users/chinyereottah/Desktop/Mac/SpatialR/data-raw/CanDEM.tif")
+#' result <- calculate_slope_aspect(elevation_raster)
+#' slope_raster <- result$slope
+#' aspect_raster <- result$aspect
+#' plot(slope_raster, main="Slope")
+#' plot(aspect_raster, main="Aspect")
+#' }
+calculate_slope_aspect <- function(dem) {
+  # Function implementation goes here
+}
+
+install.packages("roxygen2")
